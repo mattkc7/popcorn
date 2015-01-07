d5b105abefabbde18571e2c64f45b9484b561913
Matthew Chan <mattkc7@gmail.com>
2015-01-07 15:11:25 +0000

    Rails boiler plate

---
+++ popcorn/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popcorn::Application.config.secret_key_base = '71b6387a65ef6b6faf2e665658f6937d1e45e1eae8b5bf6133885c45857a3c4fd0b17eebb317c25c5753fd1c8a831c17cf63a56502199421feabed52262810e4'
