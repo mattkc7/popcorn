502f7e75b7fc191a09e67cc36f6101c2f550b486
Matthew Chan <mattkc7@gmail.com>
2015-01-07 15:28:22 +0000

    Setting up AngularJS

--- popcorn/config/routes.rb
+++ popcorn/config/routes.rb
@@ -1,4 +1,5 @@
 Popcorn::Application.routes.draw do
+  root "popcorn#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
